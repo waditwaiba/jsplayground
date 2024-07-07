71cf81e76b1cd47a7ef47fb5c7fafdadf431e840
faiyazbits <m.faiyaz123@gmail.com>
2024-07-07 15:22:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Playground from 'javascript-playgrounds'
+
+function App() {
+
+  return (
+    <>
+      <Playground style={{ width: "100vw", height: "100vh" }} />
+    </>
+  )
+}
+
+export default App
